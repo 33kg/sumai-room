4216f5f344106a5e84fa76cd917951d9e452acd2
Hirofumi Saito <hsaito@zaico.co.jp>
2020-02-27 22:08:59 +0900

    Fix: 物件画像が表示されるように修正

--- single-fudo.php
+++ single-fudo.php
@@ -15,6 +15,25 @@ global $post_id;
 global $syubetu;
 $post_id = $post->ID;
 $syubetu = FudoUtil::getSyubetu( $post_id );
+
+//物件画像取得
+$postModel = NrPostModel::getInstance();
+$thumbOptions = array();
+$largeOptions = array();
+$length = 30;
+for($i=1;$i<=$length;$i++){
+	$thumbOptions[$i] = array('imgid' => $i,'size' => 'thumbnail');
+	$largeOptions[$i] = array('imgid' => $i,'size' => 'large');
+}
+$img_infos = $postModel->getFudoImgs(array($post_id), array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30));
+$attachment_data = $postModel->getAttachimentImgDatas(array($post_id));
+$imgThumbArray = FudoUtil::fudoimgurls($post_id, $img_infos, $attachment_data, 'thumbnail');
+$imgLargeArray = FudoUtil::fudoimgurls($post_id, $img_infos, $attachment_data, 'large');
+if(my_custom_kaiin_view('kaiin_gazo', 0, 1)){
+}else{
+	FudoUtil::convertKaiinImage($imgThumbArray, 'thumbnail', 0, 1);
+	FudoUtil::convertKaiinImage($imgLargeArray, 'large', 0, 1);
+}
 ?>
 
 <div class="content-area" style="min-height:800px;">
@@ -33,7 +52,7 @@ $syubetu = FudoUtil::getSyubetu( $post_id );
 <div class="wrap-detail">
 
 	<div class="summary">
-		<img src="<?php echo get_template_directory_uri(); ?>/_dummy/aed9f95a2b16b96b01a8421fbb5715df-e1439795971398.png">
+		<img src="<?php echo $imgLargeArray[0]['img_url']; ?>">
 		<h2><?php echo FudoUtil::bukkennmei($post_id); ?></h2>
 		<?php if($syubetu == SYUBETU_CHINTAI):?>
 			<?php include("inc/bukken/inc-fudoinfo-chintai.php"); ?>
@@ -55,9 +74,9 @@ $syubetu = FudoUtil::getSyubetu( $post_id );
 	    <div class="wrap-gallery"><!--MV・商品画像-->
 		    <div class="swiper-container gallery-single">
 		        <div class="swiper-wrapper">
-					<div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/_dummy/35224855.png"></div>
-					<div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/_dummy/35224855.png"></div>
-					<div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/_dummy/35224855.png"></div>
+				<?php for( $i=1; $i < count($imgLargeArray); $i++ ): ?>
+					<div class="swiper-slide"><img src="<?php echo $imgLargeArray[$i]['img_url']; ?>"></div>
+				<?php endfor; ?>
 				</div>
 				<!-- If we need navigation buttons -->
 				<div class="wrap-button">
@@ -68,9 +87,9 @@ $syubetu = FudoUtil::getSyubetu( $post_id );
 			<!-- サムネイル -->
 			<div class="swiper-container gallery-thumbnail">
 			    <div class="swiper-wrapper">
-					<div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/_dummy/35224855.png"></div>
-					<div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/_dummy/35224855.png"></div>
-					<div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/_dummy/35224855.png"></div>
+				<?php for( $i=1; $i < count($imgLargeArray); $i++ ): ?>
+					<div class="swiper-slide"><img src="<?php echo $imgLargeArray[$i]['img_url']; ?>"></div>
+				<?php endfor; ?>
 			    </div>
 			</div>
 
