8025d1be0fe30fb797f79ae4b827634ecffce3ac
Hirofumi Saito <hsaito@zaico.co.jp>
2020-02-28 06:48:35 +0900

    Fix: 物件種別別の検索ページの作成

--- page-search-buy.php
+++ page-search-buy.php
@@ -7,7 +7,204 @@
  * @since Twenty Sixteen 1.0
  */
 
-get_header(); ?>
+get_header();
+
+// 売買物件検索用
+the_post();
+$find = isset($_GET['find'])? $_GET['find'] : '2' ;
+$postModel = NrPostModel::getInstance();
+//売買 賃貸
+$shub = 1;
+$shub_txt = ' (売買)';
+
+$site = site_url( '/' ); 
+if($shub == 1){
+	$shu_data = '< 3000' ;
+}else if($shub == 2){
+	$shu_data = '> 3000' ;
+}
+$bukkenSyubetuHash = $postModel->getBukkenSyubetu($shu_data);
+$madoriHash = $postModel->getMadoriHash($shu_data);
+$setubiHash = $postModel->getSetubi($shu_data);
+$shikuHash = $postModel->getShiku($shu_data);
+$rosenStationHash = $postModel->getRosenStation($shu_data);
+
+$tikunensuuHash = array(
+	0=>'指定なし',
+	1=>'1年以内(新築)',
+	3=>'3年以内',
+	5=>'5年以内',
+	10=>'10年以内',
+	15=>'15年以内',
+	20=>'20年以内',
+);
+$kakakuLowHash = array(
+	0=>'下限なし',
+	500=>'500万円',
+	1000=>'1000万円',
+	1500=>'1500万円',
+	2000=>'2000万円',
+	2500=>'2500万円',
+	3000=>'3000万円',
+	3500=>'3500万円',
+	4000=>'4000万円',
+	4500=>'4500万円',
+	5000=>'5000万円',
+	5500=>'5500万円',
+	6000=>'6000万円',
+	7000=>'7000万円',
+	8000=>'8000万円',
+	9000=>'9000万円',
+	10000=>'1億円',
+);
+$kakakuHighHash = array(
+	500=>'500万円',
+	1000=>'1000万円',
+	1500=>'1500万円',
+	2000=>'2000万円',
+	2500=>'2500万円',
+	3000=>'3000万円',
+	3500=>'3500万円',
+	4000=>'4000万円',
+	4500=>'4500万円',
+	5000=>'5000万円',
+	5500=>'5500万円',
+	6000=>'6000万円',
+	7000=> '7000万円',
+	8000=> '8000万円',
+	9000=> '9000万円',
+	10000=> '1億円',
+	0=> '上限なし',	
+);
+$tinryouLowHash = array(
+	0=> '下限なし',
+	3=> '3万円',
+	4=> '4万円',
+	5=> '5万円',
+	6=> '6万円',
+	7=> '7万円',
+	8=> '8万円',
+	9=> '9万円',
+	10=> '10万円',
+	11=> '11万円',
+	12=> '12万円',
+	13=> '13万円',
+	14=> '14万円',
+	15=> '15万円',
+	16=> '16万円',
+	17=> '17万円',
+	18=> '18万円',
+	19=> '19万円',
+	20=> '20万円',
+	30=> '30万円',
+	40=> '40万円',
+	50=> '50万円',
+	60=> '60万円',
+	70=> '70万円',
+	80=> '80万円',
+	90=> '90万円',
+	100=> '100万円',	
+
+);
+
+$tinryouHighHash = array(
+	3=> '3万円',
+	4=> '4万円',
+	5=> '5万円',
+	6=> '6万円',
+	7=> '7万円',
+	8=> '8万円',
+	9=> '9万円',
+	10=> '10万円',
+	11=> '11万円',
+	12=> '12万円',
+	13=> '13万円',
+	14=> '14万円',
+	15=> '15万円',
+	16=> '16万円',
+	17=> '17万円',
+	18=> '18万円',
+	19=> '19万円',
+	20=> '20万円',
+	30=> '30万円',
+	40=> '40万円',
+	50=> '50万円',
+	60=> '60万円',
+	70=> '70万円',
+	80=> '80万円',
+	90=> '90万円',
+	100=> '100万円',
+	0=> '上限なし',
+
+);
+$tohohunHash = array(
+	0=> '指定なし',
+	1=> '1分以内',
+	3=> '3分以内',
+	5=> '5分以内',
+	10=> '10分以内',
+	15=> '15分以内',	
+);
+$mensekiLowHash = array(
+	0 =>'下限なし',
+	10=> '10m&sup2;',
+	15=> '15m&sup2;',
+	20=> '20m&sup2;',
+	25=> '25m&sup2;',
+	30=> '30m&sup2;',
+	35=> '35m&sup2;',
+	40=> '40m&sup2;',
+	50=> '50m&sup2;',
+	60=> '60m&sup2;',
+	70=> '70m&sup2;',
+	80=> '80m&sup2;',
+	90=> '90m&sup2;',
+	100=> '100m&sup2;',
+	120=> '120m&sup2;',
+	140=> '140m&sup2;',
+	160=> '160m&sup2;',
+	180=> '180m&sup2;',
+	200=> '200m&sup2;',
+	300=> '300m&sup2;',
+	400=> '400m&sup2;',
+	500=> '500m&sup2;',
+	600=> '600m&sup2;',
+	700=> '700m&sup2;',
+	800=> '800m&sup2;',
+	900=> '900m&sup2;',
+	1000=> '1000m&sup2;',
+);
+$mensekiHighHash = array(
+	10=> '10m&sup2;',
+	15=> '15m&sup2;',
+	20=> '20m&sup2;',
+	25=> '25m&sup2;',
+	30=> '30m&sup2;',
+	35=> '35m&sup2;',
+	40=> '40m&sup2;',
+	50=> '50m&sup2;',
+	60=> '60m&sup2;',
+	70=> '70m&sup2;',
+	80=> '80m&sup2;',
+	90=> '90m&sup2;',
+	100=> '100m&sup2;',
+	120=> '120m&sup2;',
+	140=> '140m&sup2;',
+	160=> '160m&sup2;',
+	180=> '180m&sup2;',
+	200=> '200m&sup2;',
+	300=> '300m&sup2;',
+	400=> '400m&sup2;',
+	500=> '500m&sup2;',
+	600=> '600m&sup2;',
+	700=> '700m&sup2;',
+	800=> '800m&sup2;',
+	900=> '900m&sup2;',
+	1000=> '1000m&sup2;',
+	0=> '上限なし',
+);
+
+?>
 <div class="content-area" style="min-height:800px;">
 
 	<ul class="breadlist">
@@ -16,31 +213,16 @@ get_header(); ?>
 	</ul>
 
 	<section class="wrap-block wrap-form">
-		<h2>検索結果<span class="result-tex"><strong>10</strong>件中<strong>1〜5件</strong>表示しています</span></h2>
-		<?php include("searchform_buy.php"); //賃貸検索フォーム ?>
-		<?php include("inc_paging.php"); //ページング ?>
+		<h2>売買物件検索</h2>
+		<?php include("searchform_buy.php"); //売買検索フォーム ?>
 	</section>
 
-	<?php include("inc_search-detail_buy.php"); //検索結果一つ分 ?>
-
-	<?php //繰り返し部分  ?>
-	<?php include("inc_search-detail_buy.php"); //検索結果一つ分 ?>
-	<?php include("inc_search-detail_buy.php"); //検索結果一つ分 ?>
-	<?php include("inc_search-detail_buy.php"); //検索結果一つ分 ?>
-	<?php include("inc_search-detail_buy.php"); //検索結果一つ分 ?>
-	<?php include("inc_search-detail_buy.php"); //検索結果一つ分 ?>
-	<?php // ./繰り返し部分  ?>
-	<?php include("inc_paging.php"); //ページング ?>
 
 	<section class="wrap-block wrap-recently_seen">
-		<h2>最近見た物件</h2>
+		<h2>この他の物件</h2>
 		<ul class="recent-search">
-		<li><?php include("inc_property_rent.php"); ?></li>
-		<li><?php include("inc_property_rent.php"); ?></li>
-		<li><?php include("inc_property_rent.php"); ?></li>
-		<li><?php include("inc_property_rent.php"); ?></li>
+		<?php include("inc_others_buy.php"); ?>
 		</ul>
-		<div class="wrap-link"><a href="#">more</a></div>
 	</section>
 
 
--- searchform_buy.php
+++ searchform_buy.php
@@ -3,52 +3,37 @@
  * 検索フォーム
  */
 ?>
-<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-
+<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' )."fudo/" ); ?>" class="jsearch">
+<input type="hidden" name="bukken" value="jsearch" >
+<input type="hidden" name="shub" value="<?php echo $shub?>" >
 		<dl>
 		<dt>物件種別</dt>
 		<dd>
 			<ul class="list2">
-			<li><input id="box_a1" type="checkbox" /><label for="box_a1">売地</label></li>
-			<li><input id="box_a2" type="checkbox" /><label for="box_a2">新築戸建て</label></li>
-			<li><input id="box_a3" type="checkbox" /><label for="box_a3">中古戸建て</label></li>
-			<li><input id="box_a4" type="checkbox" /><label for="box_a4">収益物件</label></li>
-			<li><input id="box_a5" type="checkbox" /><label for="box_a5">その他</label></li>
+			<?php $i = 0; ?>
+			<?php foreach( $bukkenSyubetuHash as $key => $value ): ?>
+			<li><input type="checkbox" name="shu[]" value="<?php echo $key; ?>" id="<?php echo $key; ?>"><label for="<?php echo $key; ?>"><?php echo $value; ?></label></li>
+			<?php endforeach; ?>
 			</ul>
 		</dd>
 		<dt>地域</dt>
 		<dd>
 			<ul class="list2">
-			<li><input id="box_b1" type="checkbox" /><label for="box_b1">山形市</label></li>
-			<li><input id="box_b2" type="checkbox" /><label for="box_b2">寒河江市</label></li>
-			<li><input id="box_b3" type="checkbox" /><label for="box_b3">天童市</label></li>
-			<li><input id="box_b4" type="checkbox" /><label for="box_b4">朝日町</label></li>
-			<li><input id="box_b5" type="checkbox" /><label for="box_b5">朝日町</label></li>
-			<li><input id="box_b6" type="checkbox" /><label for="box_b6">大江町</label></li>
+			<?php foreach($shikuHash as $fudoName => $hash):?>
+				<?php foreach($hash as $key => $value):?>
+					<li><input type="checkbox" name="ksik[]"  value="<?php echo $key?>" id="<?php echo $key?>"><label for="<?php echo $key; ?>"><?php echo $value?></label></li>
+				<?php endforeach?>
+			<?php endforeach?>
 			</ul>
 		</dd>
-		<dt>賃料</dt>
+		<dt>価格</dt>
 		<dd>
 			<ul class="list">
 			<li>
 				<select name="kalb" id="kalb">
-				<option value="0">下限なし</option>
-				<option value="500">500万円</option>
-				<option value="1000">1000万円</option>
-				<option value="1500">1500万円</option>
-				<option value="2000">2000万円</option>
-				<option value="2500">2500万円</option>
-				<option value="3000">3000万円</option>
-				<option value="3500">3500万円</option>
-				<option value="4000">4000万円</option>
-				<option value="4500">4500万円</option>
-				<option value="5000">5000万円</option>
-				<option value="5500">5500万円</option>
-				<option value="6000">6000万円</option>
-				<option value="7000">7000万円</option>
-				<option value="8000">8000万円</option>
-				<option value="9000">9000万円</option>
-				<option value="10000">1億円</option>
+				<?php foreach($kakakuLowHash as $key => $value):?>
+					<option value="<?php echo $key?>"><?php echo $value?></option>
+					<?php endforeach?>
 				</select>
 		  	</li>
 			<li>
@@ -56,24 +41,11 @@
 		  	</li>
 			<li>
 				<select name="kahb" id="kahb">
-				<option value="500" >500万円</option>
-				<option value="1000" >1000万円</option>
-				<option value="1500" >1500万円</option>
-				<option value="2000" >2000万円</option>
-				<option value="2500" >2500万円</option>
-				<option value="3000" >3000万円</option>
-				<option value="3500" >3500万円</option>
-				<option value="4000" >4000万円</option>
-				<option value="4500" >4500万円</option>
-				<option value="5000" >5000万円</option>
-				<option value="5500" >5500万円</option>
-				<option value="6000" >6000万円</option>
-				<option value="7000" >7000万円</option>
-				<option value="8000" >8000万円</option>
-				<option value="9000" >9000万円</option>
-				<option value="10000" >1億円</option>
-				<option value="0" selected>上限なし</option>
-				</select>
+				<?php foreach($kakakuHighHash as $key => $value):?>
+					<?php $selected = !$key ? 'selected':'0';?>
+					<option value="<?php echo $key?>"><?php echo $value?></option>
+					<?php endforeach?>
+	          	</select>
 		  	</li>
 			</ul>
 		</dd>
@@ -82,69 +54,51 @@
 			<ul class="list">
 			<li>
 				<select name="mel" id="mel">
-				<option value="0">下限なし</option>
-				<option value="10">10m&sup2;</option>
-				<option value="15">15m&sup2;</option>
-				<option value="20">20m&sup2;</option>
-				<option value="25">25m&sup2;</option>
-				<option value="30">30m&sup2;</option>
-				<option value="35">35m&sup2;</option>
-				<option value="40">40m&sup2;</option>
-				<option value="50">50m&sup2;</option>
-				<option value="60">60m&sup2;</option>
-				<option value="70">70m&sup2;</option>
-				<option value="80">80m&sup2;</option>
-				<option value="90">90m&sup2;</option>
-				<option value="100">100m&sup2;</option>
-				<option value="120">120m&sup2;</option>
-				<option value="140">140m&sup2;</option>
-				<option value="160">160m&sup2;</option>
-				<option value="180">180m&sup2;</option>
-				<option value="200">200m&sup2;</option>
-				<option value="300">300m&sup2;</option>
-				<option value="400">400m&sup2;</option>
-				<option value="500">500m&sup2;</option>
-				<option value="600">600m&sup2;</option>
-				<option value="700">700m&sup2;</option>
-				<option value="800">800m&sup2;</option>
-				<option value="900">900m&sup2;</option>
-				<option value="1000">1000m&sup2;</option>
-				</select>
+				<?php foreach($mensekiLowHash as $key => $value):?>
+				<option value="<?php echo $key?>"><?php echo $value?></option>
+				<?php endforeach?>
+	          	</select>
 		  	</li>
 			<li>
 				〜
 		  	</li>
 			<li>
 				<select name="meh" id="meh">
-				<option value="10" >10m&sup2;</option>
-				<option value="15" >15m&sup2;</option>
-				<option value="20" >20m&sup2;</option>
-				<option value="25" >25m&sup2;</option>
-				<option value="30" >30m&sup2;</option>
-				<option value="35" >35m&sup2;</option>
-				<option value="40" >40m&sup2;</option>
-				<option value="50" >50m&sup2;</option>
-				<option value="60" >60m&sup2;</option>
-				<option value="70" >70m&sup2;</option>
-				<option value="80" >80m&sup2;</option>
-				<option value="90" >90m&sup2;</option>
-				<option value="100" >100m&sup2;</option>
-				<option value="120" >120m&sup2;</option>
-				<option value="140" >140m&sup2;</option>
-				<option value="160" >160m&sup2;</option>
-				<option value="180" >180m&sup2;</option>
-				<option value="200" >200m&sup2;</option>
-				<option value="300" >300m&sup2;</option>
-				<option value="400" >400m&sup2;</option>
-				<option value="500" >500m&sup2;</option>
-				<option value="600" >600m&sup2;</option>
-				<option value="700" >700m&sup2;</option>
-				<option value="800" >800m&sup2;</option>
-				<option value="900" >900m&sup2;</option>
-				<option value="1000" >1000m&sup2;</option>
-				<option value="0" selected>上限なし</option>
+					<?php foreach($mensekiHighHash as $key => $value):?>
+					<?php $selected = !$key ? 'selected':'0';?>
+						<option value="<?php echo $key?>" <?php echo $selected?>><?php echo $value?></option>
+					<?php endforeach?>
+	          	</select>
+		  	</li>
+			</ul>
+		</dd>
+		<dt>築年数</dt>
+		<dd>
+			<ul class="list">
+			<li>
+				<select name="tik" id="tik">
+				<?php foreach($tikunensuuHash as $key => $value):?>
+				<option value="<?php echo $key?>"><?php echo $value?></option>
+				<?php endforeach?>
 				</select>
 		  	</li>
+			</ul>
+		</dd>
+		<dt>間取り</dt>
+		<dd>
+			<ul class="list">
+			<?php foreach ( $madoriHash as $key => $value ):?>
+				<li><input name="mad[]" value="<?php echo $key; ?>" id="mad2<?php echo $key; ?>" type="checkbox"/><label for="mad2<?php echo $key; ?>"><?php echo $value; ?></label></li>
+			<?php endforeach?>
+			</ul>
+		</dd>
+		<dt>条件</dt>
+		<dd>
+			<ul class="list3">
+			<?php foreach ( $setubiHash as $key => $value ):?>
+				<li><input type="checkbox" name="set[]"  value="<?php echo $key; ?>" id="set2<?php echo $key; ?>"><label for="set2<?php echo $key; ?>"><?php echo $value; ?></label></li>
+			<?php endforeach?>
+		
 			</ul>
 		</dd>
 		</dl>
