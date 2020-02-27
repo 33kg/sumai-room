662a93627e4cf8ec18d0b85b47a3a9a10c725023
Dai Shirahata <61480845+33kg@users.noreply.github.com>
2020-02-28 05:05:38 +0900

    Add files via upload

---
+++ page-president-style.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * Used to display archive-type pages if nothing more specific matches a query.
+ * For example, puts together date-based pages if no date.php file exists.
+ *
+ * If you'd like to further customize these archive views, you may create a
+ * new template file for each one. For example, tag.php (Tag archives),
+ * category.php (Category archives), author.php (Author archives), etc.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+
+get_header(); ?>
+
+<div class="content-area president_style" style="min-height:800px;">
+
+	<ul class="breadlist">
+	<li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">住まいるーむ情報館</a></li>
+	<li><?php wp_title(''); ?></li>
+	</ul>
+
+	<div class="column-wrap">
+		<h2>PRESIDENT STYLE<span>社長の流儀</span></h2>
+		<section class="wrap-block">
+
+			<img src="<?php echo get_template_directory_uri(); ?>/images/president_style/ryugi1.png">
+
+		</section>
+		<section class="wrap-block">
+			<h3>材木業から不動産業への二次創業。人の役に立つ生き方、ビジネスを形に</h3>
+			<div class="wrap-inner">
+				<p class="tex">志田宏社長が不動産業を興したのは平成3年、23歳の時である。 当時、志田家は父親が昭和53年に起業した木材業を営んでおり、その跡を継いだものの、住宅建設における木材の需要は新建材による工法の急速な普及で激減、売上は好調時の約半分にまで落ち込んでいた。<br>
+				強い危機感のなか、志田社長はすぐに資格を得て「志田不動産」を立ち上げる。<br>
+				「小さい頃から親が手形で苦労していたのも知っていましたし、何よりも人の役に立つ生き方、仕事をしたいという思いが強かったですね」。<br>
+				木材業を兼業しながら、不動産業に身を投じた志田社長は、次々と業界の慣習を打ち破っていく。<br>
+				土日祝日の営業、お客様が気軽に立ち寄れる店舗づくり、敷金精算の入居者負担からオーナー負担への転換、そして、賃貸情報等の幅広い収集と発信。<br>
+				「今では当たり前のことになっていますが、当時はなかなか理解されませんでした」と志田社長は笑う。<br>
+				徹底して買う側、借りる側の立場にたった営業方針は、結果として成約実績を伸ばし、オーナーや売り手からの信頼に結びついていった。<br>
+				平成16年には法人化し「住まいるーむ 情報館」を設立。業績は伸び続け、毎年売上記録を塗り替えている。</p>
+			</div>
+		</section>
+
+		<section class="wrap-block">
+			<h3>深刻な空き家問題の解決に向けた、空き家ネットワークサービス</h3>
+			<div class="wrap-inner">
+				<p class="tex">「空き家は社会的な問題もありますが、 一番困っているのは持ち主の方なんです。 解体して更地にしても、その後どうすればよいか先が見えない。それでは多額の解体費用も工面のしようがありません」。<br>
+				このような実情を踏まえ、管理代行、賃貸仲介、売買業務を三本柱にした住まい再生、空き家管理サービスを開始。<br>
+				また、空き家問題の解決は、相続や税金など多岐にわたり、行政にも専門の相談窓口はない。そのため、解体業者、工務店だけでなく、税理士、弁護士、司法書士など様々なプロと連携し、ワンストップサービ スを提供できる新たな体制を整えた。<br>
+				それが「空き家相談ネットワーク」の サービスである。<br>
+				「空き家問題の解決は、地域にとって不動産業に関わる者の使命だと思っています。 売ってくれてありがとう。買ってくれてありがとう。貸してくれてありがとう。こういったお互いの気持ちを繋げていくのが 、私たちの役目なんです」。</p>
+			</div>
+		</section>
+
+		<section class="wrap-block">
+			<h3>風水師プロデュースによる分譲住宅の販売事業をスタート</h3>
+			<div class="wrap-inner">
+				<p class="tex">現在、「住まいるーむ情報館」では、前身の木材業、賃貸仲介業、住宅売買、空き家相談ネットワークのサービスなどが主な業務。これに加え「風水(ふうすい)」を全面的に取り入れた住宅の分譲を新たに始める。<br>
+				「風水はいろいろな捉え方がありますが、いわゆる環境学です。風水心理カウンセラーである私の妻が全面的に関わり、気の流れや方角の吉凶など裏付けのある設計を行います。今までにない試みですので、私自身とても期待しています。」<br>
+				家づくりだけでなく、風水セミナーの開催も視野に入れ、少しでも人の役に立ちたいと志田社長。</p>
+			</div>
+		</section>
+
+		<section class="wrap-block">
+			<h3>感動を提供するには、まず自分たちが感動を体験すること</h3>
+			<div class="wrap-inner">
+				<p class="tex">「お客様に感動されるような行動をしよう、とスタッフに常に話すんですが、なかなかできない。自分自身が感動した経験が少ないんですね。
+				そこで、社員の誕生日にサプライズを用意したり、全員で登山や敬称抜きをルール にした親睦旅行を計画するなど、感動体験をコンセプトにした社内イベントを通して 、自分たちで感動を作っていこうと」。 志田社長が大切にしていることが、もうひとつある。社長の言葉をそのまま借りると「金臭くないこと」だという 。「お金は大切ですが、それは手段であって目的ではいけない。資格も同じです。<br>
+				知識や技術をどう活かすかは、人間としての根っこの部分が大事。 それは親を大事にする、正直であるといった当たり前のことなんですが」。 だからこそ、物件の案内や成約ありきで はなく「感謝と笑顔の種を蒔く」という意識を社員に求める。<br>
+				それは、父親の口癖でもあった「知り合いに売るのは経営ではない、いかに知らない人に買ってもらうことだ」との言葉につながる。 創業者から二代目に受け継がれている経営哲学である。</p>
+			</div>
+		</section>
+
+	</div>
+	
+
+</div><!-- .content-area -->
+
+<?php get_footer(); ?>
